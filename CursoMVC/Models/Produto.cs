ed14499fb71b9fd94f58795e64a09143ea5f998b
njeronimo <adolescencia23@hotmail.com>
2022-01-27 07:06:03 -0300

    Adicionar arquivos de projeto.

---
+++ CursoMVC/Models/Produto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "O campo descrição é obrigatório")]
+        public string Descricao { get; set; }
+
+        [Range(1, 10, ErrorMessage = "O Valor tem que ser de 1 a 10")]
+        public int Quantidade { get; set; }
+
+        public int CategoriaId { get; set; }
+
+        public Categoria Categoria { get; set; }
+    }
+}
